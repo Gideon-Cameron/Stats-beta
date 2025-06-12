0141afc34d0817e8f3b36e38bb84c407817eeed4
Gideon Cameron <gideonwork20@gmail.com>
2025-06-12 03:08:28 -0700

    created and added a way to calculate the strength stats

---
+++ src/utils/calculateStrengthRank.ts
@@ -0,0 +1,17 @@
+import { strengthRankThresholds, StrengthTest, Rank } from '../data/strengthRankThresholds';
+
+export function calculateStrengthRank(test: StrengthTest, value: number): Rank {
+  const thresholds = strengthRankThresholds[test];
+
+  let matchedRank: Rank = 'E';
+
+  for (const threshold of thresholds) {
+    if (value >= threshold.min) {
+      matchedRank = threshold.rank;
+    } else {
+      break;
+    }
+  }
+
+  return matchedRank;
+}
