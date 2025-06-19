a471b6b69a961f939c6c78906d4e0dbef3a7f93b
Gideon Cameron <gideonwork20@gmail.com>
2025-06-19 05:02:06 -0700

    Removing unused code and making the project more clear

--- src/data/flexibilityRankThresholds.ts
+++ src/data/flexibilityRankThresholds.ts
@@ -1,12 +1,5 @@
-export type Rank =
-  | 'E'
-  | 'D'
-  | 'C'
-  | 'B'
-  | 'A'
-  | 'S'
-  | 'SS'
-  | 'Mythic';
+import { Rank } from '../types/Rank';
+
 
 export type FlexibilityTest =
   | 'frontSplitLeft'
