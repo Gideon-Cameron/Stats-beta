15251076c7b78fcf0fc1186e5fb8766505086f11
Gideon Cameron <gideonwork20@gmail.com>
2025-06-19 19:50:09 -0700

    Fixing import

--- src/utils/calculateFlexibilityRank.ts
+++ src/utils/calculateFlexibilityRank.ts
@@ -1,4 +1,5 @@
-import { flexibilityRankThresholds, FlexibilityTest, Rank } from '../data/flexibilityRankThresholds';
+import { flexibilityRankThresholds, FlexibilityTest, } from '../data/flexibilityRankThresholds';
+import { Rank } from '../types/Rank'
 
 export function calculateFlexibilityRank(test: FlexibilityTest, value: number): Rank {
   const thresholds = flexibilityRankThresholds[test];
