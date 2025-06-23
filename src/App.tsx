f5b59248adc42e0c56443dd70f302c90e72593c1
Gideon Cameron <gideonwork20@gmail.com>
2025-06-23 00:43:32 -0700

    Checking an error

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SpeedStatPage from './pages/stats/speed';
 import SkillStatPage from './pages/stats/skill';
 import FlexibilityStatPage from './pages/stats/flexibility';
 import StatsIndex from './pages/stats/index';
-import LoginPage from './pages/Login';
+import LoginPage from './pages/Login.tsx';
 import { useAuth } from './context/AuthContext';
 import Home from './pages/home'; // ✅ NEW HOME IMPORT
 
